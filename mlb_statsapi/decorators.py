5918a103c14ee02b33e59ae9c627cf31841d37ae
Nikhil Gupta <nikhil97gupta@gmail.com>
2023-07-05 15:21:44 -0400

    Add flag for try catch activation

--- mlb_statsapi/decorators.py
+++ mlb_statsapi/decorators.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import Any
+from dotenv import dotenv_values
 
 from .constants import MetaFields
 
@@ -8,6 +9,11 @@ from .constants import MetaFields
 # Requires a zero argument callable that we try to run as normal.
 # If f errors, return a special value
 def t(f) -> Any:
+    config = dotenv_values(".env")
+
+    if 'DEBUG' in config:
+        return f()
+
     try:
         return f()
     except (AttributeError, KeyError, TypeError, ValueError) as e:
